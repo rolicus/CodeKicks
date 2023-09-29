fa0bfdca9c323d4e5488135311b4829dc874b4c7
L4362 <145105499+L4362@users.noreply.github.com>
2023-09-29 13:53:52 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
 
+const uri = 'mongodb+srv://rolicus:CodeKicks4550@cluster0.oqn2t2i.mongodb.net/?retryWrites=true&w=majority'
+
+async function connect(){
+    try {
+        await mongoose.connect(uri)
+        console.log("Connected to MongoDB");
+    } catch (erro) {
+        console.error(error);
+    }
+}
+
+connect();
+
+app.listen(8000, () => {
+    console.log("Server started on port 8000");
+});
