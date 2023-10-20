a10015f7fc813ca1bedcc0dc1feb0f52ac33dde7
rolicus <rolicusthefirst@gmail.com>
2023-10-20 13:09:48 -0700

    addshoe

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const session = require('express-session');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    secret: 'CodeKicks4550', // Replace with a secure secret key
+    resave: false,
+    saveUninitialized: true,
+}));
 
 // Connect to the database
 mongoose.connect("mongodb+srv://rolicus:CodeKicks4550@cluster0.oqn2t2i.mongodb.net/CodeKicks", {
@@ -11,8 +17,9 @@ mongoose.connect("mongodb+srv://rolicus:CodeKicks4550@cluster0.oqn2t2i.mongodb.n
     useUnifiedTopology: true,
 });
 
+app.use(express.static('images'));
 
-/***************************************************/
+/****************************************/
 const usersSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
@@ -27,7 +34,7 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/home.html');
 });
 
-app.post("/signup", function(req, res) { // Change the form action to "/signup"
+app.post("/signup", function (req, res) {
     let newUsers = new users({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
@@ -37,7 +44,7 @@ app.post("/signup", function(req, res) { // Change the form action to "/signup"
     });
     newUsers.save()
         .then(() => {
-            res.redirect('/login.html'); // Redirect to login after signup
+            res.redirect('/login.html');
         })
         .catch((error) => {
             console.error(error);
@@ -45,7 +52,6 @@ app.post("/signup", function(req, res) { // Change the form action to "/signup"
         });
 });
 
-/***********************************************************************/
 const shoesSchema = new mongoose.Schema({
     name: String,
     price: String,
@@ -53,18 +59,17 @@ const shoesSchema = new mongoose.Schema({
 });
 
 const shoes = mongoose.model("shoes", shoesSchema);
-// Route to handle adding a shoe
-app.post("/addShoe", function(req, res) {
+
+app.post("/addShoe", isAuthenticated, function(req, res) {
     let newShoe = new shoes({
-        name: req.body.name, // Replace with the appropriate form field names
+        name: req.body.name,
         price: req.body.price,
         description: req.body.description,
-        // Add other shoe properties as needed
     });
 
     newShoe.save()
         .then(() => {
-            res.redirect('/shop.html'); // Redirect to the shop page after adding a shoe
+            res.redirect('/shop.html');
         })
         .catch((error) => {
             console.error(error);
@@ -72,14 +77,50 @@ app.post("/addShoe", function(req, res) {
         });
 });
 
+app.get("/shoes", function (req, res) {
+    // Fetch shoe data from your database
+    shoes.find({}, (err, shoes) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: "Error fetching shoe data" });
+        } else {
+            // Send the shoe data as JSON
+            res.json(shoes);
+        }
+    });
+});
 
 
-app.use(express.static('images')); // Serve static files from the 'public' directory
-app.use('images', express.static(__dirname + 'CodeKicks'));
-/***********************************************************************/
-// all these below rout links to server
+app.delete("/deleteShoe/:id", (req, res) => {
+    const shoeId = req.params.id; // Extract the shoe ID from the URL
+    
+    // Use Mongoose to delete the shoe from the database
+    shoes.findByIdAndRemove(shoeId, (err) => {
+        if (err) {
+            console.error("Failed to delete the shoe:", err);
+            res.status(500).send("Failed to delete the shoe.");
+        } else {
+            // Send a success response
+            res.sendStatus(200);
+        }
+    });
+});
+
+
+function isAuthenticated(req, res, next) {
+    if (req.session.authenticated)  {
+        next();
+    } else {
+        res.redirect('/login.html');
+    }
+}
+
 app.get('/home.html', function (req, res) {
     res.sendFile(__dirname + '/home.html');
+});
+
+app.get('/addshoe.html', isAuthenticated, function (req, res) {
+    res.sendFile(__dirname + '/addshoe.html');
 });
 
 app.get('/shop.html', function (req, res) {
@@ -103,10 +144,6 @@ app.get('/images/logo2-2.jpg', function (req, res) {
     res.sendFile(__dirname + '/images/logo2-2.jpg');
 });
 
-/*************************************************************/
-
-
-
 app.post("/login", async function(req, res) {
     const { email, password } = req.body;
 
@@ -114,6 +151,7 @@ app.post("/login", async function(req, res) {
         const foundUser = await users.findOne({ email, password }).exec();
 
         if (foundUser) {
+            req.session.authenticated = true;
             res.redirect("/home.html");
         } else {
             res.status(401).send("Invalid email or password. Please try again.");
@@ -124,87 +162,7 @@ app.post("/login", async function(req, res) {
     }
 });
 
-app.listen(5500, () => {
-    console.log("Server started on port 5500");
-});
-/*
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const app = express();
-
-app.use(bodyParser.urlencoded({extended: true}));
-
-//connects to database
-mongoose.connect("mongodb+srv://rolicus:CodeKicks4550@cluster0.oqn2t2i.mongodb.net/CodeKicks" , {useNewUrlParser: true}, {useUnifiedTopology: true},)
-
-//creates schema to send to database
-const usersSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    address: String,
-    email: String,
-    password: String,
-});
-
-const users = mongoose.model("users", usersSchema);
-//this was oringally singup.html
-// Route to fetch all products
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/home.html');
-})
-
-app.post("/", function(req,res){
-    let newUsers = new users({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        address: req.body.address,
-        email: req.body.email,
-        password: req.body.password
-    });
-    newUsers.save();
-    res.redirect('/login.html');
-})
-
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/signup.html');
-})
-
-app.use(express.static('CodeKicks'));
-
-app.get('/home.html', function (req, res) {
-    res.sendFile(__dirname + '/home.html');
-}); 
-
-app.get('/login.html', function (req, res) {
-    res.sendFile(__dirname + '/login.html');
-});
 
 app.listen(5500, () => {
     console.log("Server started on port 5500");
 });
-
-
-
-// Route to handle the login form
-app.post("/login", async function(req, res) {
-    const { email, password } = req.body;
-
-    try {
-        // Check the database for the user with the provided email and password
-        const foundUser = await users.findOne({ email, password }).exec();
-
-        if (foundUser) {
-            // User found, you can set up a session or send a success message
-            // Redirect to the home page after a successful login
-            res.redirect("/home.html");
-        } else {
-            // User not found or credentials do not match, you can send an error message
-            res.status(401).send("Invalid email or password. Please try again.");
-        }
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Internal Server Error");
-    }
-});
-*/
